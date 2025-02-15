cd9788e679fdd8689e2618b661f4bca1c0c8dc34
dongkiat <56468194+dongkiat@users.noreply.github.com>
2025-02-16 06:40:36 +0800

    Implement ingredient info generation

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from recipe import recipe_bp
+from routes.recipe import recipe_bp
+from routes.ingredient import ingredient_bp
 
 app = Flask(__name__)
 app.register_blueprint(recipe_bp)
+app.register_blueprint(ingredient_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
