c229294bde2a5f7866bcf748d0cfe1aa80822dbe
AppDevIn <teamprojectlive@gmail.com>
2025-02-16 08:05:10 +0800

    Merge image to ingredient

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes.recipe import recipe_bp
 from routes.ingredient import ingredient_bp
+from routes.imageToIngredient import imageToIngredient_bp
 
 app = Flask(__name__)
 app.register_blueprint(recipe_bp)
 app.register_blueprint(ingredient_bp)
+app.register_blueprint(imageToIngredient_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
